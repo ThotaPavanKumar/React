4e9ffa4a1d7d33dd313055260d8fb318ac4fc749
ThotaPavanKumar <pavankumarthota1@gmail.com>
2022-01-23 23:55:41 +0530

    timer

---
+++ Timer/src/Timer.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+export const Timer = () => {
+  const [count, setCount] = useState(10);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount((p) => {
+        if (p === 0) {
+          clearInterval(id);
+          return 0;
+        }
+        return p - 1;
+      });
+    }, 1000);
+
+    return () => {
+      console.log("unmounting");
+      clearInterval(id);
+    };
+  }, []);
+
+  return <div className="counter">Counter:-{count}</div>;
+};
